f4b5954be8f7ed2efbef7235f28c56c3fccc8df4
Daniel <hanakoscar@outlook.com>
2020-10-07 16:12:08 -0400

    update2

--- my-app/src/app/postings/postings.component.ts
+++ my-app/src/app/postings/postings.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Job } from '../job';
 
 @Component({
   selector: 'postings',
@@ -6,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./postings.component.scss']
 })
 export class PostingsComponent implements OnInit {
-  job = 'Front End Developer'
+  job: Job = {
+    id: 1,
+    title: 'Front End Developer',
+    company: 'Kronos',
+    level: 'Entry - Associate',
+    description: 'Very nice company and former employees vouched for its wonderful working atmosphere. The recruiter Dan was very friendly and asked normal phone screen questions. He mentioned that this job would be working primarily in Angular, hence my effort to build this app to re-familiarize myself in preparation for a potential next interview.'
+  }
   constructor() { }
 
   ngOnInit(): void {
