c1ef71e430a0fa0f7ef23588890685674dee9ca8
Daniel <hanakoscar@outlook.com>
2020-10-07 19:52:48 -0400

    update3

---
+++ my-app/src/app/jobs.ts
@@ -0,0 +1,18 @@
+import { Job } from "./job";
+
+export const JOBS: Job[] = [
+  {
+    id: 2,
+    title: 'Associate Software Engineer',
+    company: 'Corvus Insurance',
+    level: 'Associate',
+    description: 'From what I have heard from my friend Nader and Romina, who is a fellow Launch Academy alumni, this job deals with functional programming and is a really wonderful place to work. I applied and reached out to their VP of Engineering in hopes of getting an interview. I have yet to hear back.'
+  },
+  {
+    id: 3,
+    title: 'Front End Engineer',
+    company: 'Robin Powered',
+    level: 'Mid',
+    description: 'I applied to Robin based off their job description. It seemed I had all the prerequisite fields of experience, though I recently was turned down due to lack of years of experience in such fields. Regardless I feel positively towards them and I am happy that they let me know I was not being considered further for this postion.'
+  }
+]
